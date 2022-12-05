0b6489abe3a4536561ff6d89283adf5f5f4583aa
KevStu <100114266+KevStu@users.noreply.github.com>
2022-12-05 13:55:02 +0800

    Update and rename Main to Main.java

---
+++ Main.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main{
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My UI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panelTitle = new JPanel(new FlowLayout());
+        JPanel panelOne = new JPanel(new GridLayout(2,2,1,1));
+        JPanel panelTwo = new JPanel(new FlowLayout());
+        JButton button = new JButton("Click");
+
+        panelTitle.setBounds(10,10,380,40);
+        panelOne.setBounds(10,60,380,80);
+        panelTwo.setBounds(10,200,380,40);
+
+        panelTitle.setBackground(Color.white);
+        panelOne.setBackground(Color.GRAY);
+        panelTwo.setBackground(Color.pink);
+
+        JLabel labelOne, labelTwo, labelTitle;
+
+        JTextField tFieldOne;
+        tFieldOne = new JTextField(20);
+        tFieldOne.setPreferredSize(new Dimension(80,20));
+
+
+        labelTitle = new JLabel("Enter required:", JLabel.CENTER);
+        labelOne = new JLabel("First Value");
+
+
+        labelTitle.setBounds(10,10,90,20);
+        labelOne.setBounds(10,10,90,20);
+        tFieldOne.setBounds(5, 5, 100, 100);
+
+
+        panelTitle.add(labelTitle);
+
+        panelOne.add(labelOne);
+        panelOne.add(tFieldOne);
+
+
+        button.setBounds(10,10,90,20);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = computeValue(tFieldOne.getText());
+                displayMessage(String.valueOf(result));
+            }
+        });
+
+        panelTwo.add(button);
+
+        frame.add(panelTitle);
+        frame.add(panelOne);
+        frame.add(panelTwo);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int height = screenSize.height;
+        int width = screenSize.width;
+
+        frame.setSize(400,300);
+        frame.setResizable(false);
+        frame.setLayout(null);
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public static int computeValue(String value1){
+       int val1 = Integer.parseInt(value1);
+        return val1;
+    }
+    public static void displayMessage(String message){
+       JOptionPane.showMessageDialog( null, message, "Result" , JOptionPane.INFORMATION_MESSAGE );
+    }
+}
